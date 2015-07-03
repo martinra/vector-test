dca07e04faa570848bdf2b5207d5ed8996ef1264
Martin Westerholt-Raum <martin@raum-brothers.eu>
2015-07-03 12:20:17 +0200

    Correct infinite loop in QuickCheck

--- src/Test/Vector/QuickCheck.hs
+++ src/Test/Vector/QuickCheck.hs
@@ -11,9 +11,14 @@ instance (Arbitrary a) => Arbitrary (Vector a) where
   arbitrary = arbitrary >>= \(NonNegative (Small n)) ->
               V.replicateM n arbitrary
 
-  shrink as | V.null as = []
-            | otherwise =    [ as ]
-                          ++ [ V.head as `V.cons` as'
-                             | as' <- shrink (V.tail as) ]
-                          ++ [ a' `V.cons` as
-                             | a' <- shrink (V.head as) ]
+  shrink as
+    | V.null as = []
+    | l == 1    = shrinkElts
+    | otherwise = [asLeft, asRight] ++ shrinkElts
+    where
+      l = V.length as
+      (asLeft,asRight) = V.splitAt (l `div` 2) as
+      shrinkElts = [ V.update as $ V.singleton (ix,e)
+                   | ix <- [0..l-1]
+                   , e <- shrink $ as V.! ix
+                   ]
