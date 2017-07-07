65b61a2c61822d1e221a601d88aad771d17aea6f
Martin Raum <martin@raum-brothers.eu>
2017-07-07 10:57:04 +0200

    remove Natural

--- src/Test/Vector/SmallCheck.hs
+++ src/Test/Vector/SmallCheck.hs
@@ -5,15 +5,15 @@
 module Test.Vector.SmallCheck
 where
 
+import Control.Monad ( guard )
 import qualified Data.Vector as V
 import Data.Vector ( Vector )
-import Numeric.Natural ( Natural )
 
-import Test.Natural
 import Test.SmallCheck.Series
 
 
 instance (Monad m, Serial m a) => Serial m (Vector a) where
-  series = series >>= \n ->
-    V.sequence $ V.iterateN (fromIntegral (n :: Natural)) decDepth $
-    decDepth series
+  series = do
+    n <- series
+    guard $ n >=0 
+    V.sequence $ V.iterateN n decDepth $ decDepth series
